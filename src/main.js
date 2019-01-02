32bc93eeede3ab72535bbda71e0bb65a979c4676
894568474 <894568474@qq.com>
2019-01-02 14:37:12 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -6,12 +6,11 @@ import store from './store'
 import hzqTool from 'hzq-tool'
 import hzqAxios from 'hzq-axios'
 
-import apiUrl from './apiUrl'
 import './components/global'
 
 Vue.use(hzqTool)
 
-Vue.use(hzqAxios, apiUrl, {
+Vue.use(hzqAxios, require.context('@/apiUrl', true, /\.js$/), {
     baseURL: 'https://open-api.beone.app'
 })
 
